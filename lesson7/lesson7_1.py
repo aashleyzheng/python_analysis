8f60982e92a244ef000be3ea99b963b7dc20ce97
aashleyzheng <kvwzy79263@gmail.com>
2025-05-04 02:58:49 +0000

    modify

--- lesson7/lesson7_1.py
+++ lesson7/lesson7_1.py
@@ -1,6 +1,23 @@
+#參數
+def get_status(bmi:float):
+    if bmi < 18.5:
+        status = "體重過輕"
+    elif bmi < 24:
+        status = "正常"
+    elif bmi < 27:
+        status = "過重"
+    elif bmi < 30:
+        status = "輕度肥胖"
+    elif bmi <35:
+        status = "中度肥胖"
+    else:
+        status = "重度肥胖"
+    print(f"狀態:{status}")
+
 #自訂function(內建的變數)，主執行區域
 #自訂function，前面一定要有def main()
 #程式由上而下執行不會去執行def main()
+#使用的變數為區域變數
 def main(): 
     height:int = int(input("請輸入身高公分"))
     weight:int = int(input("請輸入體重公斤"))
@@ -9,21 +26,11 @@ def main():
     print(f"身高:{height}""cm")
     print(f"體重:{weight}""kg")
     print(f"BMI:{BMI}")
-
-    if BMI < 18.5:
-        status = "體重過輕"
-    elif BMI < 24:
-        status = "正常"
-    elif BMI < 27:
-        status = "過重"
-    elif BMI < 30:
-        status = "輕度肥胖"
-    elif BMI <35:
-        status = "中度肥胖"
-    else:
-        status = "重度肥胖"
-    print(f"狀態:{status}")
+    get_status(BMI)
+    
 
 #專案主要執行檔 => 名字就會叫做main
+#全域變數
+#if __name__ == '__main__': => 文件的敘述
 if __name__ == '__main__':
     main() #呼叫 def main() 這個function
